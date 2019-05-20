d0e779f07db3abaaafe4feecd1f3be38c216a40e
Ridley Neslon <43862428+Ridley-nelson17@users.noreply.github.com>
2019-05-20 07:35:20 -0500

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,16 @@
+from abc import ABCMeta, abstractmethod
+from six import add_metaclass
+
+@add_metaclass(ABCMeta)
+class StemmerI(object):
+    """A processing interface for removing morphological affixes from
+    words.  This process is known as stemming."""
+    
+    @abstractmethod
+    def stem(self, token):
+        """
+        Strip affixes from the token and return the stem.
+
+        :param token: The token that should be stemmed.
+        :type token: str
+        """
